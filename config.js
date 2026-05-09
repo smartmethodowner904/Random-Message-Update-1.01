4ccfbf318256cdc6c906440ea895ec90a8689e69
smartmethodowner904 <quotexrealid9045@gmail.com>
2026-05-10 01:40:23 +0600

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,8 @@ export const BOT_TOKEN = "8661744403:AAFvIQxQl9FBmUdxZCJm9ettf2KzAc1xHsA";
 
 export const ADMIN_ID = 8136997138;
 
-// Must join channel
 export const METHOD_CHANNEL = "@Global_Method_Channel";
 
-// Optional UI link
-export const MAIN_CHANNEL = "https://t.me/+75BQ2Qw9UZI4OTM1M";
-const DB_FILE = "./db.json";
+export const MAIN_CHANNEL = "-1002315458574";
+
+export const DB_FILE = "./db.json";
