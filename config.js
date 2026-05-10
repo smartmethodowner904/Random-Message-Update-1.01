952414b354b9ccdcbeaf45c577e5a380c61f09e5
smartmethodowner904 <quotexrealid9045@gmail.com>
2026-05-10 12:28:01 +0600

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,14 @@ export const BOT_TOKEN = "8661744403:AAFvIQxQl9FBmUdxZCJm9ettf2KzAc1xHsA";
 
 export const ADMIN_ID = 8136997138;
 
+// Global channel username
 export const METHOD_CHANNEL = "@Global_Method_Channel";
 
+// Main channel/group ID
 export const MAIN_CHANNEL = "-1002315458574";
 
+// Group ID যেখানে random message যাবে
+export const GROUP_ID = "-1003820143618";
+
+// DB File
 export const DB_FILE = "./db.json";
